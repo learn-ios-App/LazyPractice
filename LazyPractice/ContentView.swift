76065d5b2ec7ec410fc751d4704fceabb5dc0359
Aoitoridayo <s2071150@s.konan-u.ac.jp>
2023-11-27 20:31:18 +0900

    Initial Commit

---
+++ LazyPractice/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LazyPractice
+//
+//  Created by 渡邊魁優 on 2023/11/27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LazyPractice/LazyPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  LazyPracticeApp.swift
+//  LazyPractice
+//
+//  Created by 渡邊魁優 on 2023/11/27.
+//
+
+import SwiftUI
+
+@main
+struct LazyPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
